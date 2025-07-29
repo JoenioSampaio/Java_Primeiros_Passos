4c9556dd883c09773e45bca6dda281e59866d6f9
Joenio Sampaio <joenio.sampaio@gmail.com>
2025-07-29 00:17:32 -0300

    atual

---
+++ src/Estrutura_Sequencial/atividadeSaidaDeDados.java
@@ -0,0 +1,27 @@
+package Estrutura_Sequencial;
+
+public class atividadeSaidaDeDados {
+
+	public static void main(String[] args) {
+		
+		String product1 = "Computer";
+		String product2 = "Office Desk";
+		
+		byte age = 30;
+		int code = 5290;
+		char gender = 'F';
+		
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measure = 53.234567;
+		
+		System.out.println("products");
+		System.out.printf("%s, which price is $ %.2f\n", product1, price1);
+		
+		
+		
+		
+
+	}
+
+}
